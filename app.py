279545784043e964e6b88ed488c528e7d1c5d606
Georgijs <georgijs.jefimovs@edu.rtu.lv>
2024-12-09 12:32:13 +0200

    1.1

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('main.html')
 
+@app.route('/kontaktinfo')
+def kontaktinfo():
+    return render_template('kontaktinfo.html')
+
+@app.route('/pasutisana')
+def pasutisana():
+    return render_template('pasutisana.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
